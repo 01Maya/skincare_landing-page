fb47671c55ef4de0824d48545ffa10cf6882e0dd
Mansi_Patel <patelmaya9812@gmail.com>
2025-10-15 00:00:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Olea Skin Care',
+  description: 'Olea Skin Care Landing page-Clean and Minimal',
 }
 
 export default function RootLayout({
